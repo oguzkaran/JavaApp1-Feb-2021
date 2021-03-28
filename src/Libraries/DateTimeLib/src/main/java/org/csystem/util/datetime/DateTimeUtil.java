af43a24b06d65fcc7bf1394a326f85cae6a92bc9
Oğuz Karan <oguzkaran@csystem.org>
2021-03-28 20:44:31 +0300

    JavaApp1-Feb-2021

--- src/Libraries/DateTimeLib/src/main/java/org/csystem/util/datetime/DateTimeUtil.java
+++ src/Libraries/DateTimeLib/src/main/java/org/csystem/util/datetime/DateTimeUtil.java
@@ -1,27 +1,55 @@
+/*----------------------------------------------------------------------
+    FILE        : DateTimeUtil.java
+    AUTHOR      : Oğuz Karan
+    LAST UPDATE : 28.03.2021
+
+    Utility class for date-time operations
+
+    Copyleft (c) 1993 by C and System Programmers Association (CSD)
+    All Rights Free
+-----------------------------------------------------------------------*/
 package org.csystem.util.datetime;
 
 import java.time.*;
+import java.time.format.DateTimeFormatter;
 import java.util.Random;
 
 public final class DateTimeUtil {
+    private static final int [] DAYS_OF_MONTHS = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 32};
+
+    private static boolean isLeapYear(int year)
+    {
+        return year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
+    }
+
     private DateTimeUtil() {}
 
-    /*
+    public static final DateTimeFormatter DATE_TR = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+    public static final DateTimeFormatter DATE_DOT_TR = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    public static final DateTimeFormatter DATE_HYPHEN_TR = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+
     public static LocalDate randomLocalDate(Random r, int minYear, int maxYear)
     {
+        int year = r.nextInt(maxYear - minYear) + minYear;
+        int month = r.nextInt(12) + 1;
+        int day = r.nextInt(month == 2 && isLeapYear(year) ? 29 : DAYS_OF_MONTHS[month]) + 1;
 
+        return LocalDate.of(year, month, day);
     }
 
     public static LocalTime randomLocalTime(Random r)
     {
+        int hour = r.nextInt(24);
+        int minute = r.nextInt(60);
+        int second = r.nextInt(60);
 
+        return LocalTime.of(hour, minute, second);
     }
 
     public static LocalDateTime randomLocalDateTime(Random r, int minYear, int maxYear)
     {
         return randomLocalDate(r, minYear, maxYear).atTime(randomLocalTime(r));
     }
-    */
 
     public static long toMilliseconds(LocalDateTime localDateTime)
     {
--- src/Projects/Sample/src/main/java/org/csystem/app/App.java
+++ src/Projects/Sample/src/main/java/org/csystem/app/App.java
@@ -1,11 +1,58 @@
 /*----------------------------------------------------------------------------------------------------------------------
-
+    Sınıf Çalışması: Klavyeden sıfır girilene kadar alınan BigDecimal sayıların en büyüğünü, en küçüğünü ve toplamlarını
+    hesaplayan programı yazınız
 ----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app;
+
+import org.csystem.util.Console;
+
+import java.math.BigDecimal;
 
 class App {
     public static void main(String[] args)
     {
+        FindMaxMinTotalApp.run();
+    }
+}
+
+class FindMaxMinTotalApp {
+    private FindMaxMinTotalApp()
+    {
+    }
+
+    private static void calculate(BigDecimal initial)
+    {
+        var min = initial;
+        var max = initial;
+        var sum = initial;
+
+        for (;;) {
+            var val = Console.readBigDecimal("Bir sayı giriniz:");
+            if (val.equals(BigDecimal.ZERO))
+                break;
+
+            if (val.compareTo(min) < 0)
+                min = val;
+
+            if (max.compareTo(val) < 0)
+                max = val;
+
+            sum = sum.add(val);
+        }
+
+        Console.writeLine("En küçük sayı:%s", min);
+        Console.writeLine("En büyük sayı:%s", max);
+        Console.writeLine("Toplam:%s", sum);
+    }
+    public static void run()
+    {
+        var val = Console.readBigDecimal("Bir sayı giriniz:");
+
+        if (!val.equals(BigDecimal.ZERO))
+            calculate(val);
+        else
+            Console.writeLine("Hiç sayı girilmedi");
+
 
     }
 }
