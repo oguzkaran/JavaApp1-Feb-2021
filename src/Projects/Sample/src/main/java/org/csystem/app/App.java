3d01742b0657ead7b91a9e7299a6b3fbac25c376
Oğuz Karan <oguzkaran@csystem.org>
2021-04-17 18:11:03 +0300

    JavaApp1-Feb-2021

--- src/Projects/Sample/src/main/java/org/csystem/app/App.java
+++ src/Projects/Sample/src/main/java/org/csystem/app/App.java
@@ -1,13 +1,20 @@
 /*----------------------------------------------------------------------------------------------------------------------
-    Programın komut satırı argümanları
+    CommanLineUtil sınıfı
 ----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app;
+
+import org.csystem.util.Console;
+import org.csystem.util.StringUtil;
+
+import static org.csystem.util.CommandLineUtil.getCommandLineArgsJoin;
 
 class App {
     public static void main(String[] args)
     {
-        for (var arg : args)
-            System.out.println(arg);
+        var text = getCommandLineArgsJoin(args, "Input your text:", "");
+
+        Console.writeLine(StringUtil.isPalindrome(text) ? "Palindrom" : "Palindrom değil");
+        Console.writeLine("C and System Programmers Association");
     }
 }
 
