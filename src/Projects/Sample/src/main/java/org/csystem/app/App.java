b80c9dd195d7a8565e18e4922505618161b08148
Oğuz Karan <oguzkaran@csystem.org>
2021-05-02 19:59:52 +0300

    JavaApp1-Jan-2021

--- src/Projects/Sample/src/main/java/org/csystem/app/App.java
+++ src/Projects/Sample/src/main/java/org/csystem/app/App.java
@@ -1,49 +1,100 @@
 /*----------------------------------------------------------------------------------------------------------------------
-    Sınıf Çalışması: CountDownScheduler sınıfını genişletecek şekilde başlangıçta da bir işin yapılabilmesini
-    sağlayan onStart abstract metodunun eklendiği CountDownSchedulerEx isimli sınıfı yazınız ve test ediniz
+    assert deyiminin mesaj kısmı.
+    Not: trimToSize metodunda enlargaCapacity çağrılarını değiştirerek sonucu gözlemleyiniz
 ----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app;
 
 import org.csystem.util.Console;
-import org.csystem.util.scheduler.CountDownSchedulerEx;
 
-import java.util.concurrent.TimeUnit;
+import java.time.LocalDateTime;
+import java.util.Arrays;
 
 class App {
     public static void main(String[] args)
     {
-        CountDownSchedulerTest.run();
+        IntList list = new IntList(5);
+
+        Console.writeLine("Size:%d", list.size());
+        Console.writeLine("Capacity:%d", list.capacity());
+
+        for (int i = 0; i < 11; ++i)
+            list.add(i * 10);
+
+        Console.writeLine("Size:%d", list.size());
+        Console.writeLine("Capacity:%d", list.capacity());
+
+        list.trimToSize();
+
+        Console.writeLine("Size:%d", list.size());
+        Console.writeLine("Capacity:%d", list.capacity());
+
+        list.clear();
+
+        Console.writeLine("Size:%d", list.size());
+        Console.writeLine("Capacity:%d", list.capacity());
+
+        list.trimToSize();
+
+        Console.writeLine("Size:%d", list.size());
+        Console.writeLine("Capacity:%d", list.capacity());
+
+        list.add(10);
     }
 }
 
-final class CountDownSchedulerTest {
-    private CountDownSchedulerTest()
+class IntList {
+    private int [] m_elems;
+    private int m_index;
+
+    //...
+    private void enlargeCapacity(int newCapacity)
     {
+        assert newCapacity != 0: LocalDateTime.now();
+        assert newCapacity >= m_index: newCapacity;
+        m_elems = Arrays.copyOf(m_elems, newCapacity);
     }
-    public static void run()
+
+    public IntList(int capacity)
     {
-        var scheduler = new CountDownSchedulerEx(10, 1, TimeUnit.SECONDS) {
-            private int m_count;
+        m_elems = new int[capacity];
+    }
 
-            public void onStart()
-            {
-                Console.writeLine("Geri sayım başladı:");
-            }
+    public void add(int val)
+    {
+        if (m_elems.length == m_index)
+            enlargeCapacity(m_elems.length * 2);
 
-            public void onTick(long millisUntilFinished)
-            {
-                ++m_count;
-                Console.write("%02d\r", millisUntilFinished / 1000);
-            }
+        m_elems[m_index++] = val;
+    }
 
-            public void onFinish()
-            {
-                Console.writeLine("00");
-                Console.writeLine("Count:%d", m_count);
-                Console.writeLine("Geri sayım tamamlandı");
-            }
-        };
+    public int get(int index)
+    {
+        if (index < 0 || index >= m_index)
+            throw new IndexOutOfBoundsException("invalid index");
 
-        scheduler.startScheduler();
+        return m_elems[index];
+    }
+
+    public int capacity()
+    {
+        return m_elems.length;
+    }
+
+    public void clear()
+    {
+        m_index = 0;
+    }
+
+    public int size()
+    {
+        return m_index;
+    }
+
+    public void trimToSize()
+    {
+        enlargeCapacity(m_index);
+        //enlargeCapacity(m_index == 0 ? 1 : m_index);
     }
 }
+
+
