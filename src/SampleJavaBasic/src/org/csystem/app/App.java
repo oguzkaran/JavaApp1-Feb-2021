fc6fceaa3882b1a140cd5491ad31417fc5ee57d2
Oğuz Karan <oguzkaran@csystem.org>
2021-07-12 20:28:18 +0300

    JavaApp1-Feb-2021

--- src/SampleJavaBasic/src/org/csystem/app/App.java
+++ src/SampleJavaBasic/src/org/csystem/app/App.java
@@ -3,27 +3,12 @@
 ----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 
 class App {
-    public static void main(String[] args) throws IOException
+    public static void main(String[] args)
     {
-        BufferedReader kb = new BufferedReader(new InputStreamReader(System.in));
-
-        for (;;) {
-            System.out.print("Bir yazı giriniz:");
-            String str = kb.readLine();
-
-            if ("elma".equals(str))
-                break;
-            byte [] data = str.getBytes();
-
-            System.out.printf("Length:%d%n", data.length);
-        }
-
-        System.out.println("Tekrar yapıyor musunuz?");
+        
     }
 }
 
