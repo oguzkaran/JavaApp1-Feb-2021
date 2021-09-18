fc6bf968616386150a375976a439fc7c53cb837b
Oğuz Karan <oguzkaran@csystem.org>
2021-09-18 18:39:06 +0300

    Update

--- src/Projects/010-SpringBoot/011-RandomPasswordGeneratorScheduled/src/main/java/org/csystem/application/randomgenerator/schedule/RandomPasswordGeneratorFirst.java
+++ src/Projects/010-SpringBoot/011-RandomPasswordGeneratorScheduled/src/main/java/org/csystem/application/randomgenerator/schedule/RandomPasswordGeneratorFirst.java
@@ -1,39 +1,24 @@
-package org.csystem.application.randomgenerator.runner;
+package org.csystem.application.randomgenerator.schedule;
 
 import org.csystem.util.Console;
-import org.csystem.util.scheduler.Scheduler;
 import org.csystem.util.string.StringUtil;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.util.Random;
-import java.util.TimerTask;
 
 @Component
-public class RandomPasswordGeneratorFirst implements ApplicationRunner {
+public class RandomPasswordGeneratorFirst {
     private final BufferedWriter m_bufferedWriter;
     private final Random m_random;
-    private final Scheduler m_scheduler;
 
-    @Value("${count}")
-    private int m_count;
-
-    @Value("${timeout}")
-    private long m_timeout;
-
-    private int m_n;
-
-    private void schedulerCallback()
+    private void scheduledCallback()
     {
         try {
-            if (m_count == m_n)
-                m_scheduler.cancel();
-            ++m_n;
             m_bufferedWriter.write(StringUtil.getRandomTextEN(m_random, 10) + "\r\n");
             m_bufferedWriter.flush();
         }
@@ -43,20 +28,26 @@ public class RandomPasswordGeneratorFirst implements ApplicationRunner {
     }
 
     public RandomPasswordGeneratorFirst(@Qualifier("bufferedWriter.first") BufferedWriter bufferedWriter,
-                                        Random random,
-                                        Scheduler scheduler)
+                                        Random random)
     {
         m_bufferedWriter = bufferedWriter;
         m_random = random;
-        m_scheduler = scheduler;
     }
 
+    @Value("${count}")
+    private int m_count;
 
+    private int m_n;
 
-    @Override
-    public void run(ApplicationArguments args) throws Exception
+    @Scheduled(fixedRate = 1000)
+    public void run()
     {
-        m_scheduler.schedule(this::schedulerCallback);
+        if (m_count == m_n)
+            System.exit(1);
+
+        ++m_n;
+
+        scheduledCallback();
     }
 }
 
