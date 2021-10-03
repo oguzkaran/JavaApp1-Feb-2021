743f95579119c7d9aff10a57b898797c546a68af
Oğuz Karan <oguzkaran@csystem.org>
2021-10-03 18:00:57 +0300

    Update

---
+++ src/Projects/010-SpringBoot/013-SampleTodoAppJPAMapStructAppContext/src/main/java/org/csystem/application/rest/todo/data/repository/ITodoRepository.java
@@ -0,0 +1,23 @@
+package org.csystem.application.rest.todo.data.repository;
+
+import org.csystem.application.rest.todo.data.entity.Todo;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ITodoRepository extends CrudRepository<Todo, Long> {
+    //@Query("select td from Todo td where td.completed = :completed") //select * from todos where completed = :completed"
+    Iterable<Todo> findByCompleted(boolean completed);
+
+    //@Query("select td from Todo td where td.title = :title")
+    Iterable<Todo> findByTitle(String title);
+
+    Iterable<Todo> findByTitleContains(String text);
+
+    //@Query("select td from Todo td where td.completed=:completed and td.title = :title")
+    Iterable<Todo> findByCompletedAndTitle(boolean completed, String title);
+
+    Iterable<Todo> findByCompletedAndTitleContains(boolean completed, String text);
+
+    @Query(value = "select * from todos where date_part('month', insert_date_time)=?", nativeQuery = true)
+    Iterable<Todo> findByMonth(int month);
+}
