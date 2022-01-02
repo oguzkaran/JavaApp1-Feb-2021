dcf8e8b99045d1dabf16a569ee5e04887b2be39b
Oğuz Karan <oguzkaran@csystem.org>
2022-01-02 22:57:34 +0300

    Update

---
+++ src/Projects/010-SpringBoot/022-BasicSecurity/003-PostalCodeServiceGNUsersInMemory/src/main/java/org/csystem/application/rest/postalcode/configuration/security/PostalCodeServiceSecurityConfig.java
@@ -0,0 +1,34 @@
+package org.csystem.application.rest.postalcode.configuration.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@Configuration
+public class PostalCodeServiceSecurityConfig extends WebSecurityConfigurerAdapter {
+    private final Collection<UserDetails> m_users;
+
+    public PostalCodeServiceSecurityConfig(Collection<UserDetails> users)
+    {
+        m_users = users;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception
+    {
+        var imo = auth.inMemoryAuthentication();
+
+        for (var user : m_users)
+            imo.withUser(user);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception
+    {
+        http.httpBasic().and().formLogin().disable();
+    }
+}
---
+++ src/Projects/010-SpringBoot/022-BasicSecurity/003-PostalCodeServiceGNUsersInMemory/src/main/java/org/csystem/application/rest/postalcode/configuration/user/UsersConfig.java
@@ -0,0 +1,37 @@
+package org.csystem.application.rest.postalcode.configuration.user;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+@Configuration
+public class UsersConfig {
+    @Bean
+    public Collection<UserDetails> getUsers()
+    {
+        var adminUser = User.withDefaultPasswordEncoder()
+                .username("bora")
+                .password("1234")
+                .authorities("ADMIN", "USER")
+                .build();
+
+        var normalUser = User.withDefaultPasswordEncoder()
+                .username("coskun")
+                .password("12345")
+                .authorities("USER")
+                .build();
+
+        var disabledUser = User.withDefaultPasswordEncoder()
+                .username("oguz")
+                .password("123")
+                .authorities("ADMIN")
+                .disabled(true)
+                .build();
+
+        return new ArrayList<>() {{add(adminUser); add(normalUser); add(disabledUser);}};
+    }
+}
